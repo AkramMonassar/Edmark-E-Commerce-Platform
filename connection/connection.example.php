9063372c754aa1ef167ed45212b652191174a712
unknown <ak.monassar@gmail.com>
2026-08-23 12:38:23 +0300

    Security: stop tracking connection.php and add .gitignore

---
+++ connection/connection.example.php
@@ -0,0 +1,7 @@
+<?php
+// انسخ هذا الملف باسم connection.php وعدّل القيم حسب جهازك
+define('LOCALHOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','database');
+define('DB_CHARSET','utf8');
